19126fbf2e97721c517d14de47184b63e8acf617
Julian Domselaar <juliandaniel9809@gmail.com>
2024-08-28 14:00:28 -0300

    instalando babel para jest

--- 03-create-app-vite/src/base-pruebas/02-template-string.js
+++ 03-create-app-vite/src/base-pruebas/02-template-string.js
@@ -2,5 +2,5 @@
 export function getSaludo(nombre) {
     return 'Hola ' + nombre;
 }
-
+const nombre = "Julian"
 console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
--- 03-create-app-vite/tests/base-pruebas/02-template-string.test.js
+++ 03-create-app-vite/tests/base-pruebas/02-template-string.test.js
@@ -1,6 +1,11 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string"
+
 describe('Probando la funcion getSaludo', () => {
   test('getSaludo debe de retornar "hola julian"', () => {
-    
+    const name = "Julian";
+    const message = getSaludo(name);
+
+    expect(message).toBe(`Hola ${name}`);
   })
   
 })
